59373cdf24b70e3646cfbd4e43e2a178f3696d34
szutsgroup <53616195+szutsgroup@users.noreply.github.com>
2023-07-17 11:31:03 +0200

    Update 3_haplotypecaller_filtering.sh

--- 5_mutations/3_haplotypecaller_filtering.sh
+++ 5_mutations/3_haplotypecaller_filtering.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
 
-# re-check allele frequencies for all three raw vcf files (regenerated, control and parent)
+# re-check allele frequencies for all three raw vcf files (regenerated, control and parent) with the getaf.py script
+
+BAMDIR="/Projects/Planaria/Mutations/aligned/"
+
+cd /Projects/Planaria/Mutations/hc/
+
+for i in regenerated control parent; do
+    gzip -cd ${i}_samples.raw.vcf.gz | grep -v "#" | cut -f 1,2,4,5 > ${i}_samples_position.txt
+
+    mkdir ${i}_symlinks
+    case $i in
+    
+        regenerated)
+            for j in $(${BAMDIR}/* | grep -e "Pr" -e "R" -e "Fr"); do
+                ln -s $j ${i}_symlinks/
+            done
+            ;;
+            
+        control)
+            for j in $(${BAMDIR}/* | grep -e "Pc" -e "Fc"); do
+                ln -s $j ${i}_symlinks/
+            done
+            ;;
+            
+        parent)
+            for j in $(${BAMDIR}/* | grep -e "Pr" -e "Pc"); do
+                ln -s $j ${i}_symlinks/
+            done
+            ;;
+    esac
+
+    python3 getaf.py ${i}_samples_position.txt ${i}_symlinks/ ${i}_samples_getaf.out
+done
+
+# filter the getaf output for rows that:
+# a) mean coverage across the considered samples is between 25 and 130
+# b) the minimal mapping quality in the sample where the mutation is detected is over 40
+# c) the mutation is "present" if it has at least 7 supporting reads
+# d) the mutation is only present in 1-2 samples (to include shared mutations in siblings),
+# also one or two extra sample with some support is allowed (because of possible subclonal presence in regenerants/parents)
+
+
+
+
+    
+  
