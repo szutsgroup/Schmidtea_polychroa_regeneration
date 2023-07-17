e3eed056cd708ff744ec17b7e3aa365541a85f18
szutsgroup <53616195+szutsgroup@users.noreply.github.com>
2023-07-17 13:40:32 +0200

    Update 4_isomut.py

--- 5_mutations/4_isomut.py
+++ 5_mutations/4_isomut.py
@@ -12,6 +12,32 @@ from isomut_wrappers_mod import run_isomut
 params=dict()
 params['n_min_block']=200
 params['n_conc_blocks']=12
-params['ref_fasta']="/zfs2/szuts/poti/202303_planaria_review/assembly_versions/bm_Spol_v1.fasta"
-#input dir output dir
-params['input_dir']='/zfs2/szuts/poti/202303_planaria_review/aligned/'
+params['ref_fasta']="/Projects/Planaria/PacBio/scaffolding/bm_Spol_g1.fasta"
+params['input_dir']='/Projects/Planaria/Mutations/aligned/'
+
+# defining mutation calling parameters
+
+params['min_sample_freq']=0.15
+params['min_other_ref_freq']=0.9
+params['cov_limit']=20
+params['base_quality_limit']=30
+params['min_gap_dist_snv']=0
+params['min_gap_dist_indel']=20
+
+# Isomut run 1: without Fr4A and Fc4A
+
+params['output_dir']='/Projects/Planaria/Mutations/IsoMut/1/'
+params['bam_filenames']=list(map(os.path.basename, glob.glob(params['input_dir'] + "/*bam")))
+params['bam_filenames'].remove("Fr4A_RMdup.bam")
+params['bam_filenames'].remove("Fc4A_RMdup.bam")
+run_isomut(params)
+
+# Isomut run 1: without Fr4B and Fc4B
+
+params['output_dir']='/Projects/Planaria/Mutations/IsoMut/2/'
+params['bam_filenames']=list(map(os.path.basename, glob.glob(params['input_dir'] + "/*bam")))
+params['bam_filenames'].remove("Fr4B_RMdup.bam")
+params['bam_filenames'].remove("Fc4B_RMdup.bam")
+run_isomut(params)
+
+# this results in two-two SNV and indel out tables in /Projects/Planaria/Mutations/IsoMut/1/ and /Projects/Planaria/Mutations/IsoMut/2/
